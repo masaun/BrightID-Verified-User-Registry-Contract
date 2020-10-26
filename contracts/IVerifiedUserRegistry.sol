4b1e2d5638e832b436da03d323d766651fcbece0
masaun <masanoriuno@gmail.com>
2020-10-27 08:44:17 +0900

    Add the IVerifiedUserRegistry.sol as a interface of the registry of verified users

---
+++ contracts/IVerifiedUserRegistry.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.8; 
+pragma experimental ABIEncoderV2; 
+  
+/** 
+* @dev Interface of the registry of verified users. 
+*/ 
+interface IVerifiedUserRegistry { 
+
+    function isVerifiedUser(address _user) external view returns (bool); 
+
+}
