6a775022e59a3e50db8d96a91893a834bf5909f2
masaun <masanoriuno@gmail.com>
2020-10-28 22:29:50 +0900

    Add clrfund into /contracts

---
+++ contracts/clrfund/IRecipientRegistry.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.8;
+pragma experimental ABIEncoderV2;
+
+/**
+ * @dev Interface of the recipient registry.
+ *
+ * This contract must do the following:
+ *
+ * - Add recipients to the registry.
+ * - Allow only legitimate recipients into the registry.
+ * - Assign an unique index to each recipient.
+ * - Find the recipient's index by their address.
+ * - Limit the maximum number of entries according to MACI's maxVoteOptions.
+ * - Remove invalid entries.
+ * - Prevent indices from changing during the funding round.
+ */
+interface IRecipientRegistry {
+
+  function addRecipient(address _recipient, string calldata _name) external;
+
+  function removeRecipient(address _recipient) external;
+
+  function getRecipientIndex(address _recipient, uint256 _timestamp) external view returns (uint256);
+
+}
