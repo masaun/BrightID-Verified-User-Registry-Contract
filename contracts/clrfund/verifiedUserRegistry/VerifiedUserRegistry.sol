c94d3a150ae2627ee74f525591cc71a6fb1e39b6
masaun <masanoriuno@gmail.com>
2020-10-29 08:56:29 +0900

    Add VerifiedUserRegistry.sol

---
+++ contracts/clrfund/verifiedUserRegistry/VerifiedUserRegistry.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.8;
+pragma experimental ABIEncoderV2;
+
+/**
+ * @dev Interface of the registry of verified users.
+ */
+contract VerifiedUserRegistry {
+
+    function isVerifiedUser(address _user) external view returns (bool) {
+        /// [In progress]: Add logics into here.
+
+    };
+
+}
