dca0fb68473ff8e807d20d74ba285cc32e6a3c19
masaun <masanoriuno@gmail.com>
2020-10-29 09:08:15 +0900

    Import IBrightID.sol into VerifiedUserRegistry.sol

--- contracts/clrfund/verifiedUserRegistry/VerifiedUserRegistry.sol
+++ contracts/clrfund/verifiedUserRegistry/VerifiedUserRegistry.sol
@@ -1,10 +1,19 @@
 pragma solidity ^0.5.8;
 pragma experimental ABIEncoderV2;
 
+import "../../BrightID-SmartContract/v4/IBrightID.sol";
+
+
 /**
  * @dev - The contract of the registry of verified users.
  */
 contract VerifiedUserRegistry {
+
+    IBrightID public iBrightID;
+
+    constructor(address _iBrightID) public {
+        iBrightID = IBrightID(_iBrightID);
+    }
 
     function isVerifiedUser(address _user) external view returns (bool) {
         /// [In progress]: Add logics into here.
