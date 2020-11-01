edde3cb54d9316e1876004da1348f7d32903c451
masaun <masanoriuno@gmail.com>
2020-11-01 23:37:43 +0900

    Update README

--- contracts_archive/BrightID-SmartContract/v4/IBrightID.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.6.3;
-
-interface IBrightID {
-    event Verified(address indexed addr);
-    function verifications(address addr) external view returns (uint);
-    function history(address addr) external view returns (address);
-}
